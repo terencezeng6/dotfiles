941cdc3bf2754fba8c8b9544f95a210cebfabe57
Terence Zeng <terencezeng2004@gmail.com>
2026-08-01 14:29:47 -0700

    rofi use drun instead of run

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -29,7 +29,7 @@ hl.monitor({
 -- Set programs that you use
 local terminal    = "kitty"
 local fileManager = "dolphin"
-local menu        = "rofi -show run"
+local menu        = "rofi -show drun"
 
 
 -------------------
